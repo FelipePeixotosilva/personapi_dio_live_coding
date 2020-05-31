b6d1f9b030fa7794cc4a9f71605eb54e5606676e
felipe <felipe.peixoto@msn.com>
2020-05-31 14:11:53 -0300

    Implementando PersonService

---
+++ src/main/java/one/digitalInnovation/personapi/service/PersonService.java
@@ -0,0 +1,36 @@
+package one.digitalInnovation.personapi.service;
+
+
+import one.digitalInnovation.personapi.dto.response.MessageResponseDTO;
+import one.digitalInnovation.personapi.entity.Person;
+import one.digitalInnovation.personapi.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*Controlador que vai ser acessado atraves deuma api rest*/
+@RestController
+//caminho de acesso prinicipal da api Nivel 1
+@RequestMapping("/api/v1/people")
+public class PersonService {
+    private PersonService personService;
+    //Operacao de http do tipo Get
+    //@GetMapping
+    /*
+     * Teste Unitario
+     * */
+    @Autowired
+    public PersonService(PersonRepository personRepository){
+
+        this.personService=personService;
+    }
+
+    @PostMapping
+    //Resquesty ele ta vindo requisição do tipo pessoa
+    public MessageResponseDTO createPerson(@RequestBody Person person){
+    return personService.createPerson(person);
+    }
+}
