3b4a1068d61fc5f959887a22d01327b05a52233b
felipe <felipe.peixoto@msn.com>
2020-05-31 08:21:33 -0300

    Implementando a classe Phone e Person

--- src/main/java/one/digitalInnovation/personapi/entity/Person.java
+++ src/main/java/one/digitalInnovation/personapi/entity/Person.java
@@ -1,4 +1,41 @@
 package one.digitalInnovation.personapi.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import  java.time.LocalDate;
+import  java.util.List;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)//unique CPF unico, fica mais facil a INDEXIÇÃO, fica mais perfomatica
+    private String cpf;
+
+    private LocalDate birthDate;
+    /*Relacionamento de dados Pessoa relaciona telefone
+    ID->celular
+    ID->Casa
+    ID->commercial
+    Estrategia de obtenção de dados "FetchType" LAZY usado para perfomance,
+    "cascade" Cadastra telefone  na hora de fazer cadastro pessoa
+    * */
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    private List<Phone> phones;
 }
--- src/main/java/one/digitalInnovation/personapi/entity/Phone.java
+++ src/main/java/one/digitalInnovation/personapi/entity/Phone.java
@@ -1,4 +1,31 @@
 package one.digitalInnovation.personapi.entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import one.digitalInnovation.personapi.enums.PhoneType;
+
+import javax.persistence.*;
+
+//Reconecer pelo banco de dados como entidade
+@Entity
+//Get e Set não precisa ser implementado
+@Data
+//Padrao de projeto para construção de objetos
+@Builder
+//Inserir Construtores
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Phone {
+    @Id//chave primaria
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)//Estrategia de geracao de chave primaria, IDENTITY vai ser incremental
+    private Long id;
+
+    @Enumerated(EnumType.STRING)//
+    @Column(nullable = false)//Ele seja cadastrado seja obrigatorio, ou seja notnull
+    private PhoneType type;
+
+    @Column(nullable = false)
+    private String number;
 }
